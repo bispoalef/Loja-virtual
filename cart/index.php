a261f8db8a5f1471838ae1a9869b0bebaa5da745
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-09-20 20:33:20 -0300

    salvando assets e criando corpo do site

---
+++ cart/index.php
@@ -0,0 +1,29 @@
+<?php
+ob_start();
+require('./sheep_core/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Loja</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<!-- Topo do Site -->
+
+<div class="header">
+    <p class="logo">Loja Prototipo</p>
+    <div class="cart">
+        <p><i class="fa fa-shopping-cart"></i> 0</p>
+    </div>
+</div>
+
+<!-- Fim do Site --> 
+
+</body>
+</html>
