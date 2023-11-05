107a0678a1b54f82140d55ea8077267b41ccdbb8
AlefBispo <alef.b.dos.santos.1993@gmail.com>
2023-11-05 20:53:28 -0300

    Aplicando logica para detalhes do produto

--- cart/Produtos/produtos.js
+++ cart/Produtos/produtos.js
@@ -2,27 +2,41 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("mock.json")
         .then(response => response.json())
         .then(data => {
-            const carousel = document.getElementById("carousel");
-            data.forEach(product => {
-                const carouselItem = document.createElement("div");
-                carouselItem.classList.add("carousel-item", "carousel-animation");
+            const carousel = document.querySelector(".carousel-inner");
 
+            data.forEach((product, index) => {
+                const carouselItem = document.createElement("div");
+                carouselItem.classList.add("carousel-item");
+                if (index === 1) {
+                    carouselItem.classList.add("active");
+                }
+            
                 const productImage = new Image();
                 productImage.src = product.image;
                 productImage.alt = product.name;
-                productImage.classList.add("product-image");
-                productImage.style.maxWidth = "200px";
-
-                carouselItem.appendChild(productImage);
-                carouselItem.innerHTML += `
+                productImage.style.borderRadius= "50%"
+                productImage.classList.add("d-block", "max-width-100", "max-height-100");
+            
+                const itemContent = document.createElement("div");
+                itemContent.classList.add("carousel-caption");
+                itemContent.style.color = "#333"; 
+                itemContent.style.padding = "10px";
+                itemContent.innerHTML = `
                     <h3>${product.name}</h3>
                     <p>${product.description}</p>
                     <p>Preço: R$ ${product.price.toFixed(2)}</p>
-                    <a href="../PaginaProduto/index.html">Detalhes</a>
+                    <a href="../PaginaProduto/?id=${product.id}">Detalhes</a>
                 `;
+            
+                const carouselInner = document.createElement("div");
+                carouselInner.classList.add("d-flex", "justify-content-between");
+                carouselInner.appendChild(productImage);
+                carouselInner.appendChild(itemContent);
+            
+                carouselItem.appendChild(carouselInner);
                 carousel.appendChild(carouselItem);
             });
-
+            
 
             const productList = document.getElementById("product-list");
             data.forEach(product => {
@@ -31,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <h2>${product.name}</h2>
                     <p>${product.description}</p>
                     <p>Preço: R$ ${product.price.toFixed(2)}</p>
-                    <a href="../PaginaProduto/index.html">Detalhes</a>
+                    <a href="../PaginaProduto/?id=${product.id}">Detalhes</a>
                 `;
                 productList.appendChild(listItem);
             });
